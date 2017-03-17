ac4c0e78087af8650eecf6e4b8578e475fc08fad
Thais Faria <thais_ods@hotmail.com>
2017-03-17 01:26:28 -0300

    correct commit

---
+++ contato.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contato</title>
+
+	<!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="padroes.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+  <?php include 'header.php';?>
+
+	<div class="container">
+		<div class="jumbotron">
+  			<div class="container">
+
+	    		<!-- h1 e p que já tínhamos -->
+	    		<h1>Contato</h1>
+	    		<p>Entre em contato conosco :)</p>
+
+	    		<form>
+	    			<div class="form-group">
+	    				<label for="nome">Nome:</label>
+	    				<input type="text" class="form-control" id="nome">
+	    			</div>
+	    			<div class="form-group">
+	    				<label for="email">Email:</label>
+	    				<input type="email" class="form-control" id="email">
+	    			</div>
+	    			<div class="form-group">
+	    				<label for="assunto">Assunto:</label>
+	    				<input type="text" class="form-control" id="assunto">
+	    			</div>
+	    			<div class="form-group">
+	    				<label for="mensagem">Mensagem:</label>
+	    				<textarea class="form-control" rows="5" id="mensagem"></textarea>
+	    			</div>
+	    			<button class="btn btn-default" type="submit">Enviar</button>
+	    			<button class="btn btn-danger" onclick="history.go(-1);">Voltar</button>
+	    		</form>
+
+			</div><!-- fim .container dentro do jumbotron -->
+		</div>
+	</div>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+
+function echoActiveClassIfRequestMatches($requestUri) {
+	$current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
+
+	if ($current_file_name == $requestUri) {
+		echo 'class="active"';
+	}
+
+}
+
+?>
+<nav class="navbar navbar-default">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php">Notícias da Hora</a>
+		</div>
+		<div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li <?=echoActiveClassIfRequestMatches("index")?> >
+					<a href="index.php">Destaques</a>
+				</li>
+				<li <?=echoActiveClassIfRequestMatches("politica")?> >
+					<a href="politica.php">Política</a>
+				</li>
+				<li <?=echoActiveClassIfRequestMatches("esportes")?> >
+					<a href="esportes.php">Esportes</a>
+				</li>
+				<li <?=echoActiveClassIfRequestMatches("seguranca")?> >
+					<a href="seguranca.php">Segurança</a>
+				</li>
+				<li <?=echoActiveClassIfRequestMatches("contato")?> >
+					<a href="contato.php">Contato</a>
+				</li>
+			</ul>
+		</div><!--/.nav-collapse -->
+	</div>
+</nav>
